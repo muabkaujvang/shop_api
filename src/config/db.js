5d170a28520ec5d6a9c67a972bfc2357b682ec98
muabkaujvang <161990987+muabkaujvang@users.noreply.github.com>
2024-12-01 09:44:32 +0000

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.HOST,
+    timezone: "+07:00",
+    dialectOptions: {
+      // Your mysql2 options here
+    },
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(async () => {
+    await sequelize.sync();
+    console.log("Database has been connected");
+  })
+  .catch((error) => {
+    console.error("Unable to connect database:", error);
+  });
+
+module.exports = sequelize;
